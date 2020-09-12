4efba3b115db5cb92eb185fef6bbb117ae8a0b50
Thomas Tønder <thomas@byskov.name>
2020-09-12 19:56:50 +0200

    Working iteration, poor numbering

--- FrontEnd.Tests/EdgesFunction.fs
+++ FrontEnd.Tests/EdgesFunction.fs
@@ -10,10 +10,25 @@ let main =
         Statement.While (
             Comparison ((Loc (Identifier "x")), Greater, IntLiteral 0),
             ([],[
-                Statement.Assign(Location.Identifier "y", ArithmeticBinary ((Loc (Identifier "x")), Multiply, (Loc (Identifier "y"))))
+                Statement.Assign(Location.Identifier "y", ArithmeticBinary ((Loc (Identifier "x")), Multiply, (Loc (Identifier "y")))) ;
                 Statement.Assign(Location.Identifier "x", ArithmeticBinary ((Loc (Identifier "x")), Subtract, (IntLiteral 1)))
             ])
-        )
+        ) ;
+        Statement.Assign(Location.Identifier "b", ArithmeticExpr.IntLiteral 10)
     ])
-       
+    
+    //let ast = ([], [
+    //    Statement.Assign(Location.Identifier "a", ArithmeticExpr.IntLiteral 10) ;
+    //    Statement.If(
+    //        Comparison ((Loc (Identifier "x")), Greater, (Loc (Identifier "y"))),
+    //        ([],[
+    //            Statement.Assign(Location.Identifier "x", ArithmeticBinary ((Loc (Identifier "x")), Subtract, (IntLiteral 1)))
+    //        ]),
+    //        Some ([],[
+    //            Statement.Assign(Location.Identifier "y", ArithmeticBinary ((Loc (Identifier "y")), Subtract, (IntLiteral 1)))
+    //        ])
+    //    ) ;
+    //    Statement.Assign(Location.Identifier "b", ArithmeticExpr.IntLiteral 10)
+    //])
+    
     runEdges ast 
--- FrontEnd/EdgesFunction.fs
+++ FrontEnd/EdgesFunction.fs
@@ -6,76 +6,51 @@ open FrontEnd.ProgramGraph;
 
 module EdgesFunction = 
 
-    let rec edges (stmts : Statement List) (startIndex : int) (endIndex : int) (nList : Node List) (eList : Edge List) =
+    let rec edges (stmts : Statement List) (startNode : int) (nodeIndex : int) (nList : Node List) (eList : Edge List) =
         if stmts.IsEmpty then
-            (stmts, startIndex, startIndex, nList, eList)
+            (stmts, startNode, nodeIndex, nList, eList)
         else 
             match stmts with
                 | Statement.Assign(loc, aExpr) :: tail ->
-                    if tail.IsEmpty then
-                        edges tail (startIndex+1) endIndex nList ((startIndex, Assign (AssignExpr (loc, aExpr)), endIndex) :: eList)
-                    else
-                        edges tail (startIndex+1) endIndex (startIndex+1 :: nList) ((startIndex, Assign (AssignExpr (loc, aExpr)), startIndex+1) :: eList)
+                    edges tail nodeIndex (nodeIndex+1) (nodeIndex :: nList) ((startNode, Assign (AssignExpr (loc, aExpr)), nodeIndex) :: eList)
                     
                 | StructAssign(id, faExprL) :: tail ->
-                    if tail.IsEmpty then
-                        edges tail (startIndex+1) endIndex nList ((startIndex, AssignLiteral (AssignLiteralExpr (id, faExprL)), endIndex) :: eList)
-                    else
-                        edges tail (startIndex+1) endIndex (startIndex+1 :: nList) ((startIndex, AssignLiteral (AssignLiteralExpr (id, faExprL)), startIndex+1) :: eList)
+                    edges tail nodeIndex (nodeIndex+1) (nodeIndex :: nList) ((startNode, AssignLiteral (AssignLiteralExpr (id, faExprL)), nodeIndex) :: eList)
                     
                 | If(bExpr, block, None) :: tail ->
                     let (_, bStmts) = block
-                    let (_, blockEnd, _, nList, eList) = edges bStmts (startIndex+1) endIndex (startIndex+1 :: nList) ((startIndex, Condition bExpr, startIndex+1) :: eList)
-                
-                    //Fix branch endings
-                    let blockEnd = blockEnd+1
-                    let nList = blockEnd :: nList
-                    let (n1, act, _) = eList.Head
-                    let eList = (n1, act, blockEnd) :: eList.Tail
+                    let (_, nodeIndex, _, nList, eList) = edges bStmts nodeIndex (nodeIndex+1) (nodeIndex :: nList) ((startNode, Condition bExpr, nodeIndex) :: eList)
+
+                    let (fromNode, act, _) = eList.Head //Fix last edge of the loop
+                    edges tail nodeIndex (nodeIndex+1) (nodeIndex :: nList) ((startNode, Condition (BooleanUnary (Not, bExpr)), nodeIndex) :: (fromNode, act, nodeIndex) :: eList)
                     
-                    let eList = (startIndex, Condition (BooleanUnary (Not, bExpr)), blockEnd) :: eList
-                    edges tail blockEnd endIndex nList eList
+                | If(bExpr, block, Some blockOp) :: tail ->
+                    let (_, bStmts) = block
+                    let (_, nodeIndex, _, nList, eList) = edges bStmts nodeIndex (nodeIndex+1) (nodeIndex :: nList) ((startNode, Condition bExpr, nodeIndex) :: eList)
                     
-                //| If(bExpr, block, Some blockOp) :: tail ->
-                //    let eList = (startIndex, Condition bExpr, startIndex+1) :: eList //Create edge into branch 1
-                //    let (_, bStmts) = block
-                //    let startIndex = startIndex+1
-                //    let (_, block1OutIndex, endIndex, nList, eList) = edges bStmts startIndex endIndex nList eList
-                //    
-                //    let eList =  (currentIndex, BooleanUnary (Not, bExpr), block1OutIndex) :: eList //Create edge into branch 2
-                //    let (_, bStmts) = blockOp
-                //    let (_, block2OutIndex, nList, eList) = edges bStmts block1OutIndex nList eList
-                //    
-                //    let nList = block2OutIndex::nList
-                //    
-                //     //Find last edges of both blocks, redirect to toIndex
-                //    let eList = fixBranchLastEdges eList [] Set.ofList [block1OutIndex-1, block2OutIndex-1] block2OutIndex
-                //
-                //    edges tail block2From+1 nList eList
-                
+                    let (_, bStmts) = blockOp
+                    let (_, NextNode, _, nList, eList) = edges bStmts (nodeIndex+1) (nodeIndex+2) (nodeIndex+1 :: nList) ((startNode, Condition (BooleanUnary (Not, bExpr)), nodeIndex+1) :: eList)
+                    
+                    let (fromNode, act, _) = eList.Head //Fix last edge of the loop
+                    edges tail nodeIndex NextNode nList.Tail ((fromNode, act, nodeIndex) :: eList.Tail)
+                    
                 | While(bExpr, block) :: tail ->
                     let (_, bStmts) = block
-                    let (_, _, toIndex, nList, eList) = edges bStmts (startIndex+1) startIndex (startIndex+1 :: nList) ((startIndex, Condition bExpr, startIndex+1) :: eList)
+                    let (_, nodeIndex, _, nList, eList) = edges bStmts nodeIndex (nodeIndex+1) (nodeIndex :: nList) ((startNode, Condition bExpr, nodeIndex) :: eList)
                     
-                    edges tail toIndex (toIndex+1) (toIndex::nList) ((startIndex, Condition (BooleanUnary (Not, bExpr)), toIndex) :: eList)
+                    let (fromNode, act, _) = eList.Head //Fix last edge of the loop                            
+                    edges tail nodeIndex (nodeIndex+1) (nodeIndex :: nList.Tail) ((startNode, Condition (BooleanUnary (Not, bExpr)), nodeIndex) :: (fromNode, act, startNode) :: eList.Tail)
                 
                 | Statement.Read(loc) :: tail ->
-                    if tail.IsEmpty then
-                        edges tail (startIndex+1) endIndex nList ((startIndex, Read loc, endIndex) :: eList)
-                    else
-                        edges tail (startIndex+1) endIndex (startIndex+1 :: nList) ((startIndex, Read loc, startIndex+1) :: eList)
+                    edges tail nodeIndex (nodeIndex+1) (nodeIndex :: nList) ((startNode, Read loc, nodeIndex) :: eList)
                     
                 | Statement.Write(aExpr) :: tail ->
-                    if tail.IsEmpty then
-                        edges tail (startIndex+1) endIndex nList ((startIndex, Write aExpr, endIndex) :: eList)
-                    else
-                        edges tail (startIndex+1) endIndex (startIndex+1 :: nList) ((startIndex, Write aExpr, startIndex+1) :: eList)
+                    edges tail nodeIndex (nodeIndex+1) (nodeIndex :: nList) ((startNode, Write aExpr, nodeIndex) :: eList)
                     
     let runEdges (ast : AST) =
         let (_, stmts) = ast
-        let (_, _, _, nList, eList) = edges stmts 0 -1 [0] []
-        printfn "%A" (List.rev nList)
-        printfn "%A" (List.rev eList)
-        
+        let (_, _, _, nList, eList) = edges stmts 0 1 [0] []
+        //printfn "%A" (List.rev nList)
+        //printfn "%A" (List.rev eList)
         (List.rev nList, List.rev eList)
      
